1b5b415a599c1b4562645472510753d50ea273ed
anamux <anaamux@gmail.com>
2023-03-13 18:34:28 -0300

    Implementação da regra dos botões Limpar e Calcular.

--- app/src/main/java/com/anamuxfeldt/appgaseta/view/MainActivity.java
+++ app/src/main/java/com/anamuxfeldt/appgaseta/view/MainActivity.java
@@ -2,9 +2,8 @@ package com.anamuxfeldt.appgaseta.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,9 +14,12 @@ import com.anamuxfeldt.appgaseta.R;
 import com.anamuxfeldt.appgaseta.apoio.UtilGasEta;
 
 public class MainActivity extends AppCompatActivity {
-    EditText txtGasolina, txtAlcool;
+    EditText txtGasolina, txtEtanol;
     Button btnCalcular, btnLimpar, btnSalvar,btnFinalizar;
     TextView txtResultado;
+
+    double precoEtanol, precoGasolina;
+    String recomendacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gaseta);
 
         txtGasolina = findViewById(R.id.txtGasolina);
-        txtAlcool = findViewById(R.id.txtAlcool);
+        txtEtanol = findViewById(R.id.txtEtanol);
         txtResultado = findViewById(R.id.txtResultado);
 
 
@@ -37,6 +39,31 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                boolean isDadosOk = true;
+
+                if(TextUtils.isEmpty(txtGasolina.getText())){
+                    txtGasolina.setError("Obrigatório digitar o valor da Gasolina...");
+                    txtGasolina.requestFocus();
+                    isDadosOk = false;
+                }
+                if(TextUtils.isEmpty(txtEtanol.getText())){
+                    txtEtanol.setError("Obrigatório digitar o valor da Etanol...");
+                    txtEtanol.requestFocus();
+                    isDadosOk = false;
+                }
+                if(isDadosOk){
+                    precoGasolina = Double.parseDouble(txtGasolina.getText().toString());
+                    precoEtanol = Double.parseDouble(txtEtanol.getText().toString());
+
+                    recomendacao = UtilGasEta.calcularMelhorOpcao(precoGasolina,precoEtanol);
+
+                    txtResultado.setText(recomendacao);
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Digite os dados obrigatórios.", Toast.LENGTH_SHORT).show();
+                }
+
 
             }
         });
@@ -51,6 +78,10 @@ public class MainActivity extends AppCompatActivity {
         btnLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                txtEtanol.setText("");
+                txtGasolina.setText("");
+                txtResultado.setText("RESULTADO");
 
             }
         });
