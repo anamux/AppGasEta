898b0eb4ee52ceea95efb2cd09bc58fe794c5f59
anamux <anaamux@gmail.com>
2023-03-13 16:59:37 -0300

    Criação dos objetos que receberão imput de dados.

--- app/src/main/java/com/anamuxfeldt/appgaseta/view/MainActivity.java
+++ app/src/main/java/com/anamuxfeldt/appgaseta/view/MainActivity.java
@@ -5,34 +5,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.anamuxfeldt.appgaseta.R;
 import com.anamuxfeldt.appgaseta.apoio.UtilGasEta;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtGasolina, txtAlcool;
+    Button btnCalcular, btnLimpar, btnSalvar,btnFinalizar;
+    TextView txtResultado;
 
-   // public static final int TIME_OUT=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaseta);
 
-        Toast.makeText(MainActivity.this,UtilGasEta.calcularMelhorOpcao(5.12, 3.39),
-                Toast.LENGTH_LONG).show();
-        //comutarTela();
-    }
+        txtGasolina = findViewById(R.id.txtGasolina);
+        txtAlcool = findViewById(R.id.txtAlcool);
+        txtResultado = findViewById(R.id.txtResultado);
 
-   /* private void comutarTela() {
 
-        new Handler().postDelayed(new Runnable() {
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent telaPrincipal = new Intent(MainActivity.this,
-                        GasEtaActivity.class);
-                startActivity(telaPrincipal);
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte Sempre!", Toast.LENGTH_SHORT).show();
                 finish();
             }
-        }, TIME_OUT);
-    }*/
+        });
+
+
+
+
+
+        Toast.makeText(MainActivity.this,UtilGasEta.calcularMelhorOpcao(5.12, 3.39),
+                Toast.LENGTH_LONG).show();
+
+    }
+
+
 }
